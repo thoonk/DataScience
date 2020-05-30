9e29eafa9e3a0fe62edd56d1e923f614991bbc4a
thoonk <dkrmrm878@gmail.com>
2020-05-30 15:31:10 +0900

    Add assignment1_regression.py

---
+++ assignment1_regression.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+df = pd.read_csv("im/mac_Average_daily_airQualityInfo_2018.csv")
+df.rename(columns= {"측정일자" : "cdate", "권역코드" : "acode", "권역명" : "aname", "측정소코드":"scode",
+                    "측정소명":"sname" , "미세먼지(㎍/㎥)":"fdust", "초미세먼지(㎍/㎥)":"ufdust",
+                    "오존(ppm)":"ozone", "이산화질소농도(ppm)":"nd", "일산화탄소농도(ppm)":"cm", "아황산가스농도(ppm)":"sagas"}, inplace=True)
+#print(df.columns)
+
+df_delete = df[df.fdust != 0]
+print(df_delete)
+df_delete.to_csv("data1.csv", mode='w')
+
+"""
+def read_data(file):
+    f = open(file, 'r')
+    data = csv.reader(f)
+    for line in data:
+        print(line)
+    return data
+"""
